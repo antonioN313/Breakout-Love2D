04def6d06e5a5b60fc3996d447609bbf8f7d04aa
antonioN313 <47403160+antonioN313@users.noreply.github.com>
2024-05-06 21:10:53 -0300

    update: Health and score system and StateMachine

--- src/states/PlayState.lua
+++ src/states/PlayState.lua
@@ -1,18 +1,14 @@
 PlayState = Class{__includes = BaseState}
 
-function PlayState:init()
-    self.paddle = Paddle()
-    self.ball = Ball(1)
+function PlayState:enter(params)
+    self.paddle = params.paddle
+    self.bricks = params.bricks
+    self.health = params.health
+    self.score = params.score
+    self.ball = params.ball
 
-    
     self.ball.dx = math.random(-200, 200)
     self.ball.dy = math.random(-50, -60)
-
-    
-    self.ball.x = VIRTUAL_WIDTH / 2 - 4
-    self.ball.y = VIRTUAL_HEIGHT - 42
-
-    self.bricks = LevelMaker.createMap()
 end
 
 function PlayState:update(dt)
@@ -50,6 +46,8 @@ function PlayState:update(dt)
 
         if brick.inPlay and self.ball:collides(brick) then
 
+            self.score = self.score + 10
+
             brick:hit()
 
             if self.ball.x + 2 < brick.x and self.ball.dx > 0 then
@@ -70,6 +68,24 @@ function PlayState:update(dt)
         end
     end
 
+    if self.ball.y >= VIRTUAL_HEIGHT then
+        self.health = self.health - 1
+        gSounds['hurt']:play()
+
+        if self.health == 0 then
+            gStateMachine:change('game-over', {
+                score = self.score
+            })
+        else
+            gStateMachine:change('serve', {
+                paddle = self.paddle,
+                bricks = self.bricks,
+                health = self.health,
+                score = self.score
+            })
+        end
+    end
+
 
     if love.keyboard.wasPressed('escape') then
         love.event.quit()
@@ -81,8 +97,12 @@ function PlayState:render()
     for k, brick in pairs(self.bricks) do
         brick:render()
     end
+
     self.paddle:render()
     self.ball:render()
+
+    renderScore(self.score)
+    renderHealth(self.health)
     
     if self.paused then
         love.graphics.setFont(gFonts['large'])
