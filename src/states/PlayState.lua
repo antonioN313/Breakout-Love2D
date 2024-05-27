fe32d30dd925b0f0d08bdf9466a1a4b84bc00908
antonioN313 <antoniocarlosfilho2003@gmail.com>
2024-05-27 13:47:24 -0300

    In progress: recover lives shrink and grow paddle and powerups

--- src/states/PlayState.lua
+++ src/states/PlayState.lua
@@ -9,11 +9,83 @@ function PlayState:enter(params)
     self.ball = params.ball
     self.level = params.level
 
+    self.recoverPoints = params.recoverPoints
+    self.growPoints = params.growPoints
+
+    self.Pup = Powerup(1)
+    self.keyUp = Powerup(2)
+    self.balltwo = Ball()
+    self.ballthree = Ball()
+
+    self.balltwo.skin = math.random(7)
+    self.ballthree.skin = math.random(7)
+
     self.ball.dx = math.random(-200, 200)
     self.ball.dy = math.random(-50, -60)
+
+    self.BallPup = false
+    self.PupTimer = 0
+    self.Kup = false
+    self.KupTimer = 0
+    self.balltwo.x = VIRTUAL_WIDTH / 2 - 2
+    self.balltwo.y = VIRTUAL_HEIGHT / 2 - 2
+    self.ballthree.x = VIRTUAL_WIDTH / 2 - 2
+    self.ballthree.y = VIRTUAL_HEIGHT / 2 - 2
+
+    self.balltwo.dx = math.random(-200, 200)
+    self.balltwo.dy = math.random(-50, -60)
+    self.ballthree.dx = math.random(-200, 200)
+    self.ballthree.dy = math.random(-50, -60)
 end
 
 function PlayState:update(dt)
+
+    for k, brick in pairs(self.bricks) do
+        if brick.keyPlay then
+          AllowKeyPowerUp = true
+        end
+      end
+    
+      if Ponce then
+      self.PupTimer = self.PupTimer + dt
+    else
+      self.PupTimer = 0
+    end
+    
+      if Konce and AllowKeyPowerUp then
+        self.KupTimer = self.KupTimer + dt
+      else
+        self.KupTimer = 0
+      end
+    
+        if self.PupTimer > PupSpawn then
+          PupPlay = true
+          Ponce = false
+        end
+    
+      if PupPlay then
+        self.Pup:update(dt)
+      end
+    
+      if self.KupTimer > KPupSpawn then
+        KupPlay = true
+        Konce = false
+      end
+    
+      if KupPlay then
+        self.keyUp:update(dt)
+      end
+    
+      if self.Pup:collides(self.paddle) then
+        PupPlay = false
+        self.BallPup = true
+      end
+    
+      if self.keyUp:collides(self.paddle) then
+        KupPlay = false
+        self.Kup = true
+      end    
+
     if self.paused then
         if love.keyboard.wasPressed('space') then
             self.paused = false
@@ -48,10 +120,26 @@ function PlayState:update(dt)
 
         if brick.inPlay and self.ball:collides(brick) then
 
-            self.score = self.score + (brick.tier * 200 + brick.color * 25)
-
+            if brick.keyPlay and self.Kup then
+                self.score = self.score + (brick.tier * 500 + brick.color * 25)
+                brick:hit()
+            elseif brick.solidPlay then
+                self.score = self.score + (brick.tier * 200 + brick.color * 25)
             brick:hit()
+            end
+            if self.score > self.recoverPoints then
+                self.health = math.min(3, self.health + 1)
+                self.recoverPoints = self.recoverPoints + math.min(100000, self.recoverPoints * 2)
 
+                gSounds['recover']:play()
+            end
+
+            if self.score > self.growPoints then
+              self.paddle.size = math.min(4, self.paddle.size + 1)
+              self.paddle.width = math.min(128, self.paddle.width + 32)
+              self.growPoints = self.growPoints + math.min(100000, self.growPoints * 2)
+            end
+            
             if self:checkVictory() then
                 gSounds['victory']:play()
 
